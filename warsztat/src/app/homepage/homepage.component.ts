03d26f5028aa5fd7a0edc8848066d588a606bec8
Grzegorz <grzeslach8@gmail.com>
2024-04-07 02:57:52 +0200

    homepage actual reapirs

--- warsztat/src/app/homepage/homepage.component.ts
+++ warsztat/src/app/homepage/homepage.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit, Output } from '@angular/core';
 import { Repair } from '../repair';
+import { LoginapiService } from '../loginapi.service';
+import { response } from 'express';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-homepage',
@@ -12,13 +15,8 @@ export class HomepageComponent implements OnInit {
 
   public actualRepairs: Repair[] = [];
 
-
-
-
-
-
   constructor(
-
+    public apiService: LoginapiService,
   ) {
   
    }
@@ -26,7 +24,46 @@ export class HomepageComponent implements OnInit {
     this.showModal = !this.showModal;
   }
   ngOnInit() {
- 
+    this.getRepairs();
+  }
+
+  getRepairs() {
+    this.apiService.getRepairs().subscribe({
+      next: (response: any) => {
+        this.actualRepairs = [];
+        this.apiService.triggerRefresh();
+        response.forEach((el: any) => {
+          combineLatest([
+            this.apiService.getClient(el.client_id),
+            this.apiService.getCar(el.car_id),
+            this.apiService.getMechanic(el.mechanic_id)
+          ]).subscribe({
+            next: ([clientData, carData, mechanicData]) => {
+
+              const rp: Repair = {
+                id: el.id,
+                descript: el.descript,
+                status: el.status,
+                mechanic: mechanicData[0],
+                client: clientData[0],
+                car: carData[0],
+                entryDate: new Date(el.entry_date),
+                leaveDate: new Date(el.leave_date)
+              };
+              this.actualRepairs.push(rp);
+            },
+            error: (error: any) => {
+              console.log(error);
+            }
+            
+          });
+        });
+      },
+      
+      error: (error: any) => {
+        console.log(error);
+      }
+    });
   }
  
 }
